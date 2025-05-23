daa9e7d1c9131305b708dd052f90b62a552df737
Benjamin Šukalo <bsukalo@outlook.com>
2025-05-23 23:56:34 +0200

    Edit formatting

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/header/Header";
 import Greeting from "./greeting/Greeting";
 import Skills from "./skills/Skills";
@@ -6,50 +6,51 @@ import Footer from "../components/footer/Footer";
 import ScrollToTopButton from "./topbutton/Top";
 import Profile from "./profile/Profile";
 import SplashScreen from "./splashScreen/SplashScreen";
-import {splashScreen} from "../portfolio";
-import {StyleProvider} from "../contexts/StyleContext";
-import {useLocalStorage} from "../hooks/useLocalStorage";
+import { splashScreen } from "../portfolio";
+import { StyleProvider } from "../contexts/StyleContext";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 import "./Main.scss";
 
 const Main = () => {
-  const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
-  const [isDark, setIsDark] = useLocalStorage("isDark", darkPref.matches);
-  const [isShowingSplashAnimation, setIsShowingSplashAnimation] =
-    useState(true);
+	const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
+	const [isDark, setIsDark] = useLocalStorage("isDark", darkPref.matches);
+	const [isShowingSplashAnimation, setIsShowingSplashAnimation] =
+		useState(true);
 
-  useEffect(() => {
-    if (splashScreen.enabled) {
-      const splashTimer = setTimeout(
-        () => setIsShowingSplashAnimation(false),
-        splashScreen.duration
-      );
-      return () => {
-        clearTimeout(splashTimer);
-      };
-    }
-  }, []);
+	useEffect(() => {
+		if (splashScreen.enabled) {
+			const splashTimer = setTimeout(
+				() => setIsShowingSplashAnimation(false),
+				splashScreen.duration
+			);
+			return () => {
+				clearTimeout(splashTimer);
+			};
+		}
+	}, []);
 
-  const changeTheme = () => {
-    setIsDark(!isDark);
-  };
+	const changeTheme = () => {
+		setIsDark(!isDark);
+	};
 
-  return (
-    <div className={isDark ? "dark-mode" : null}>
-      <StyleProvider value={{isDark: isDark, changeTheme: changeTheme}}>
-        {isShowingSplashAnimation && splashScreen.enabled ? (
-          <SplashScreen />
-        ) : (
-          <>
-            <Header />
-            <Greeting />
-            <Skills />
-            <Footer />
-            <ScrollToTopButton />
-          </>
-        )}
-      </StyleProvider>
-    </div>
-  );
+	return (
+		<div className={isDark ? "dark-mode" : null}>
+			<StyleProvider value={{ isDark: isDark, changeTheme: changeTheme }}>
+				{isShowingSplashAnimation && splashScreen.enabled ? (
+					<SplashScreen />
+				) : (
+					<>
+						<div className="content">
+							<Header />
+							<Greeting />
+							<Footer />
+							<ScrollToTopButton />
+						</div>
+					</>
+				)}
+			</StyleProvider>
+		</div>
+	);
 };
 
 export default Main;
