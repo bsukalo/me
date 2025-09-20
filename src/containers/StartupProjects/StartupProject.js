d42ee1eca117295e7baafb678f9cadd9931e11ec
bsukalo <bsukalo@outlook.com>
2025-09-20 23:47:07 +0200

    Update project card layout

--- src/containers/StartupProjects/StartupProject.js
+++ src/containers/StartupProjects/StartupProject.js
@@ -44,69 +44,72 @@ export default function StartupProject() {
 									}
 								>
 									<div className="project-detail">
-										<h5
-											className={
-												isDark
-													? "dark-mode card-title"
-													: "card-title"
-											}
-										>
-											{project.projectName}
-										</h5>
-										<ul className="stack-icons">
-											{project.stack?.map(
-												(stackItem, i) => (
-													<li
-														key={i}
-														className="stack-icon-inline"
-														name={
-															stackItem.skillName
-														}
-													>
-														<i
-															className={
-																stackItem.fontAwesomeClassname
+										<div>
+											<h5
+												className={
+													isDark
+														? "dark-mode card-title"
+														: "card-title"
+												}
+											>
+												{project.projectName}
+											</h5>
+											<ul className="stack-icons">
+												{project.stack?.map(
+													(stackItem, i) => (
+														<li
+															key={i}
+															className="stack-icon-inline"
+															name={
+																stackItem.skillName
 															}
-														></i>
-													</li>
-												)
-											)}
-										</ul>
-
-										<p
-											className={
-												isDark
-													? "dark-mode card-subtitle"
-													: "card-subtitle"
-											}
-										>
-											{project.projectDesc}
-										</p>
-										{project.footerLink ? (
-											<div className="project-card-footer">
-												{project.footerLink.map(
-													(link, i) => {
-														return (
-															<span
-																key={i}
+														>
+															<i
 																className={
-																	isDark
-																		? "dark-mode project-tag"
-																		: "project-tag"
+																	stackItem.fontAwesomeClassname
 																}
-																onClick={() =>
-																	openUrlInNewTab(
-																		link.url
-																	)
-																}
-															>
-																{link.name}
-															</span>
-														);
-													}
+															></i>
+														</li>
+													)
 												)}
-											</div>
-										) : null}
+											</ul>
+											<p
+												className={
+													isDark
+														? "dark-mode card-subtitle"
+														: "card-subtitle"
+												}
+											>
+												{project.projectDesc}
+											</p>
+										</div>
+										<div className="project-button">
+											{project.footerLink ? (
+												<div className="project-card-footer">
+													{project.footerLink.map(
+														(link, i) => {
+															return (
+																<span
+																	key={i}
+																	className={
+																		isDark
+																			? "dark-mode project-tag"
+																			: "project-tag"
+																	}
+																	onClick={() =>
+																		openUrlInNewTab(
+																			link.url
+																		)
+																	}
+																>
+																	{link.name}
+																</span>
+															);
+														}
+													)}
+												</div>
+											) : null}
+										</div>
 									</div>
 								</div>
 							);
